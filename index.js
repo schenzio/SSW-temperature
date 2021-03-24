56f820e95f17d021b96dc0c9f04284e67467b372
schenzio <matteo.scanna1999@gmail.com>
2021-03-24 11:50:49 +0100

    2 files modified

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 import "./style.css"; 
 
 var cityElems = Array.from(document.getElementsByClassName("città"));
+console.log(cityElems);
+var cities = [];
 for (let elem of cityElems ) {
+  cities.push(elem.innerHTML);
   elem.onclick = () => display(elem.innerHTML);
   elem.onmouseover = () => overColor(elem);
   elem.onmouseleave = () => leaveColor(elem);
 }
+document.getElementById("calcolaTM").onclick = () => tempMedia(cities);
 
 // Funzione collegata ai bottoni
 // "window" necessario in StackBlitz, può essere
@@ -42,3 +46,31 @@ function display(city) {
   // Applico il metodo send (al termine chiamerà il callback "onload")
   request.send();
 };
+
+//calcolo la temperatura media
+function tempMedia(citiesList){
+  var nCities = citiesList.length;
+  for (var city of citiesList){
+    var sommaTemp = 0;
+    let request = new XMLHttpRequest(sommaTemp);
+    request.onload = function() {
+      if (request.status === 200) {
+        var dataObject = JSON.parse(request.response);
+        var temp = dataObject.main.temp;
+        sommaTemp+=temp;
+        document.getElementById("risposta").innerText = "La temperatura media a Genova, Torino, Milano, Pisa e Roma è " + sommaTemp/nCities + "gradi";
+      } else {
+        document.getElementById("risposta").innerText = "Errore";
+     }
+   };
+   // Applico il metodo "open"
+    request.open(
+      "GET",
+      "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
+      city,
+      true
+    );
+    // Applico il metodo send (al termine chiamerà il callback "onload")
+    request.send();
+  };
+}
